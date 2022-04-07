b9c22885b95e17e75f3b58242dbdb149cb0729f8
Andrea.cavalieri <andrea.cavalieri.1@gmail.com>
2022-04-07 22:49:20 +0000

    bug fix su logout

--- gateway/src/main/webapp/app/shared/auth/private-route.tsx
+++ gateway/src/main/webapp/app/shared/auth/private-route.tsx
@@ -36,7 +36,7 @@ export const PrivateRouteComponent = ({ component: Component, hasAnyAuthorities 
       ) : (
         <Redirect
           to={{
-            pathname: '/oauth2/authorization/oidc',
+            pathname: '/',
             search: props.location.search,
             state: { from: props.location },
           }}
